4bd1bfec19017dc9f69184ba119475dbaccac563
IPogorelova <pisseversk@mail.ru>
2019-03-13 09:55:10 +0300

    Header elements' positions are edited

--- src/blocks/wrapper/wrapper.js
+++ src/blocks/wrapper/wrapper.js
@@ -3,6 +3,6 @@ $(function(){
     const rightPart = $('.wrapper_z-index_bottom');
     let tl = new TimelineMax();
 
-    tl.to(leftPart, 1, {width: '58%', repeat: -1, repeatDelay: 4, yoyo: true});
+    tl.to(leftPart, 1, {width: '58%', 'background-position:'  ,  repeat: -1, repeatDelay: 4, yoyo: true});
     tl.to(rightPart, 1, {width: '42%', repeat: -1, repeatDelay: 4, yoyo: true}, "-=1");
 });
