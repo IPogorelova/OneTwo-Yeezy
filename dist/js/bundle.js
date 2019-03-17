d3fbc7dc738c0becaba39c5a9f2cd11771eb0fc7
IPogorelova <pisseversk@mail.ru>
2019-03-17 10:54:21 +0300

    Wrappers' animation is done

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -2,10 +2,14 @@
 
 $(function () {
   var leftPart = $('.wrapper_z-index_top');
+  var leftPartImg = $('.wrapper_z-index_top .wrapper__background-image');
   var rightPart = $('.wrapper_z-index_bottom');
+  var rightPartImg = $('.wrapper_z-index_bottom .wrapper__background-image');
   var tl = new TimelineMax();
   tl.to(leftPart, 1, {
-    width: '58%',
+    css: {
+      width: '58%'
+    },
     repeat: -1,
     repeatDelay: 4,
     yoyo: true
@@ -16,5 +20,14 @@ $(function () {
     repeatDelay: 4,
     yoyo: true
   }, "-=1");
+  tl.to(leftPartImg, 1, {
+    css: {
+      left: '-100px'
+    },
+    repeat: -1,
+    delay: 1.5,
+    repeatDelay: 4,
+    yoyo: true
+  }, '-=2');
 });
 //# sourceMappingURL=bundle.js.map
--- src/blocks/wrapper/wrapper.js
+++ src/blocks/wrapper/wrapper.js
@@ -1,8 +1,11 @@
 $(function(){
     const leftPart = $('.wrapper_z-index_top');
+    const leftPartImg = $('.wrapper_z-index_top .wrapper__background-image');
     const rightPart = $('.wrapper_z-index_bottom');
+    const rightPartImg = $('.wrapper_z-index_bottom .wrapper__background-image');
     let tl = new TimelineMax();
 
-    tl.to(leftPart, 1, {width: '58%', 'background-position:'  ,  repeat: -1, repeatDelay: 4, yoyo: true});
+    tl.to(leftPart, 1, {css: {width: '58%'},  repeat: -1, repeatDelay: 4, yoyo: true});
     tl.to(rightPart, 1, {width: '42%', repeat: -1, repeatDelay: 4, yoyo: true}, "-=1");
+    tl.to(leftPartImg, 1, {css: {left: '-100px'}, repeat: -1, delay: 1.5, repeatDelay: 4, yoyo: true}, '-=2');
 });
